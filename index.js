a0894a0e49c00b156ac9e9fc9dfc26997f41f0eb
Dan Ryan <danryanfl@yahoo.com>
2015-06-10 14:24:07 -0400

    rename npm package

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var ProgressBar = require('progress');
 
 module.exports = function(numItems) {
-  return new ProgressBar('Processing [:bar] :percent :etas', {total: numItems});
+  return new ProgressBar('Processing :current of :total [:bar] :etas', {total: numItems});
 };
